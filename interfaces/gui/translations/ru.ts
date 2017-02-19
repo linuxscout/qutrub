7a2f69c86302953aa3322e7a72f42ead12132a90
linuxscout <taha_zerrouki@hotmail.com>
2017-02-19 19:25:40 +0100

    add russian translation

---
+++ interfaces/gui/translations/ru.ts
@@ -0,0 +1,393 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="de">
+<context>
+    <name>Dialog</name>
+    <message encoding="UTF-8">
+        <location filename="setting.py" line="102"/>
+        <source>تفضيلات</source>
+        <translation type="finished">Предпочтения</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="setting.py" line="103"/>
+        <source>خط عرض النتائج</source>
+        <translation type="finished">Шрифт результатов</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="setting.py" line="104"/>
+        <source>تعديل</source>
+        <translation type="finished">Поправка</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="setting.py" line="105"/>
+        <source>استعادة الخط الافتراضي</source>
+        <translation type="finished">Восстановление шрифта по умолчанию</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="setting.py" line="106"/>
+        <source>لغة التطبيق</source>
+        <translation type="finished">Язык приложений</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="setting.py" line="107"/>
+        <source>العربية</source>
+        <translation type="finished">Арабский</translation>
+    </message>
+    <message>
+        <location filename="setting.py" line="108"/>
+        <source>English</source>
+        <translation type="finished">Английский</translation>
+    </message>
+    <message>
+        <location filename="setting.py" line="109"/>
+        <source>Deutch</source>
+        <translation type="finished">Немецкий</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="setting.py" line="110"/>
+        <source>Français</source>
+        <translation type="finished">Французский</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="setting.py" line="111"/>
+        <source>أردو</source>
+        <translation type="finished">Урду</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="setting.py" line="112"/>
+        <source>فارسي</source>
+        <translation type="finished">Персидский</translation>
+    </message>
+    <message>
+        <location filename="setting.py" line="113"/>
+        <source>Japanese</source>
+        <translation type="finished">Японский</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="setting.py" line="115"/>
+        <source>البحث دائما في معجم الأفعال الثلاثية</source>
+        <translation type="finished">Всегда искать в словаре тройных глаголов</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="setting.py" line="116"/>
+        <source>إظهار علامات التشكيل بلون مختلف</source>
+        <translation type="finished">Показать диакритические знаки с разным цветом</translation>
+    </message>
+    <message>
+        <location filename="setting.py" line="114"/>
+        <source>Malay</source>
+        <translation type="finished">малайский</translation>
+    </message>
+</context>
+<context encoding="UTF-8">
+    <name>MainWindow</name>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="452"/>
+        <source>قطرب: تصريف الأفعال العربية</source>
+        <translation type="finished">Котроб: спряжение арабских глаголов</translation>
+    </message>
+    <message>
+        <location filename="qutrubgui.py" line="454"/>
+        <source>,</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="455"/>
+        <source>الماضي</source>
+        <translation type="finished">Прошедшое время</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="456"/>
+        <source>المضارع</source>
+        <translation type="finished">Настоящие время</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="457"/>
+        <source>الأمر</source>
+        <translation type="finished">Повелительное наклонение</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="502"/>
+        <source>المبني للمجهول</source>
+        <translation type="finished">пассивная форма</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="476"/>
+        <source>&lt;html dir=&apos;rtl&apos;&gt;
+&lt;body&gt;
+&lt;ol&gt;
+  &lt;li&gt;اكتب الفعل مشكولا شكلا تاما
+(الحركات والشدة ) في خانة الفعل مثال
+:
+كَتَبَ، كَاتَبَ. &lt;/li&gt;
+  &lt;li&gt;ملاحظة إذا كان الفعل مهموز
+الأول على وزن فاعل،مثل آخى يرجى كتابته على الشكل ءَاخَى. &lt;/li&gt;
+  &lt;li&gt;إذا كان الفعل ثلاثيا حدد
+حركة عين الفعل في المضارع، مثلا كتب يكتُب تأخذ الحركة ضمة في المضارع.
+إذا كان الفعل غير ثلاثي، تجاهل هذه الميزة. &lt;/li&gt;
+  &lt;li&gt;حدد اللزوم والتعدي للفعل، &lt;/li&gt;
+  &lt;li&gt;اختر الزمن الذي تريد
+التصريف فيه &lt;/li&gt;
+  &lt;li&gt;اضغط على &quot;صرّف الفعل&quot;.&lt;/li&gt;
+&lt;/ol&gt;
+&lt;/body&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="477"/>
+        <source>المضارع المنصوب والمجزوم</source>
+        <translation type="finished">Глагол настоящего времени в винительном падеже</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="478"/>
+        <source>المؤكد </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="483"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;ae_AlMateen&apos;; font-size:14pt; font-weight:600; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;حدد اللزوم والتعدي للفعل&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="finished">Выберите переходный или непереходный глагол</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="484"/>
+        <source>متعدي</source>
+        <translation type="finished">Переходный глагол</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="489"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;ae_AlMateen&apos;; font-size:16pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;اختر الزمن الذي تريد التصريف فيه&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="finished">Выбирайте время для спряжения глаголов</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="490"/>
+        <source>كل الأزمنة</source>
+        <translation type="finished">Все времена</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="495"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;ae_AlMateen&apos;; font-size:18pt; font-weight:600; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;صرف الفعل&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="496"/>
+        <source>تصريف</source>
+        <translation type="finished">Спряжение</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="501"/>
+        <source>المبني للمعلوم</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="503"/>
+        <source>خصائص الفعل</source>
+        <translation type="finished">Свойства глагола</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="504"/>
+        <source>القيمة</source>
+        <translation type="finished">Значение</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="506"/>
+        <source>ملف</source>
+        <translation type="finished">Файл</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="507"/>
+        <source>عرض</source>
+        <translation type="finished">Вид</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="508"/>
+        <source>مساعدة</source>
+        <translation type="finished">Помощь</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="509"/>
+        <source>تحرير</source>
+        <translation type="finished">Редактировать</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="510"/>
+        <source>أدوات</source>
+        <translation type="finished">Инструменты</translation>
+    </message>
+    <message>
+        <location filename="qutrubgui.py" line="511"/>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="512"/>
+        <source>ت&amp;صدير</source>
+        <translation type="finished">Экспорт</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="513"/>
+        <source>&amp;خروج</source>
+        <translation type="finished">Выход</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="514"/>
+        <source>خط</source>
+        <translation type="finished">Шрифт</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="516"/>
+        <source>حول البرنامج</source>
+        <translation type="finished">О программе</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="517"/>
+        <source>دليل الاستعمال</source>
+        <translation type="finished">Руководство по использованию</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="518"/>
+        <source>نسخ</source>
+        <translation type="finished">Копировать</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="758"/>
+        <source>من هو قطرب؟</source>
+        <translation type="finished">Кто такой Котроб?</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="520"/>
+        <source>تفضيلات</source>
+        <translation type="finished">Предпочтения</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="521"/>
+        <source>طباعة...</source>
+        <translation type="finished">Печать</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="522"/>
+        <source>معاينة الطباعة</source>
+        <translation type="finished">Предварительный просмотр</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="523"/>
+        <source>إعداد الصفحة</source>
+        <translation type="finished">Параметры Страницы</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="524"/>
+        <source>تكبير</source>
+        <translation type="finished">Увеличение</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="525"/>
+        <source>تصغير</source>
+        <translation type="finished">Уменьшение</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="497"/>
+        <source>حركة عين الثلاثي في المضارع</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="498"/>
+        <source>خيارات</source>
+        <translation type="finished">Варианты</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="499"/>
+        <source>البحث في المعجم</source>
+        <translation type="finished">Искать в словаре</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="537"/>
+        <source>الفعل  غير صالح</source>
+        <translation type="finished">Глагол недействительный</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="1005"/>
+        <source>خطأ</source>
+        <translation type="finished">Ошибка</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="702"/>
+        <source>لا شيء يمكن طبعه، صرّف أولا</source>
+        <translation type="finished">Ничто не может быть напечатан,,спрягайте глагол сначала</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="742"/>
+        <source>عذرا</source>
+        <translation type="finished">Извините</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="743"/>
+        <source>غير متوفر حاليا</source>
+        <translation type="finished">Недоступен в настоящее время</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="785"/>
+        <source>لا يمكن فتح ملف المساعدة</source>
+        <translation type="finished">Невозможно открыть файл справки</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="813"/>
+        <source>عن برنامج قطرب</source>
+        <translation type="finished">О программе Котроб</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="853"/>
+        <source>حفظ ملف</source>
+        <translation type="finished">Сохранить файл</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="868"/>
+        <source>لا بيانات للتصدير، صرّف أولا</source>
+        <translation type="finished">Нет данных для экспорта,спрягайте глагол сначала</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="873"/>
+        <source>اسم ملف غير مناسب %s</source>
+        <translation type="finished">Название Файда не подходит</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="883"/>
+        <source>لا يمكن فتح الملف %s</source>
+        <translation type="finished">Невозможно открыть файл</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="886"/>
+        <source>لا يمكن حفظ الملف %s</source>
+        <translation type="finished">Невозможно сохранить файл</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="933"/>
+        <source>الفعل %s غير صالح2</source>
+        <translation type="finished">Глагол недействительный</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="956"/>
+        <source>اختر زمنا واحدا على الأقل</source>
+        <translation type="finished">Выберите по крайней мере одно время</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="1006"/>
+        <source>الفعل غير موجود في قائمة الأفعال الثلاثية</source>
+        <translation type="finished">Глагол недоступен в листе тройных глаголов</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qutrubgui.py" line="789"/>
+        <source>دليل الاستخدام</source>
+        <translation type="finished">Руководство пользователя</translation>
+    </message>
+</context>
+</TS>
