9bcdf3c4854c11debeee10bc9b8d229e19a035f1
linuxscout <taha_zerrouki@hotmail.com>
2021-11-11 00:07:29 +0100

    fix suggestions geenration

--- core/adaat.py
+++ core/adaat.py
@@ -50,77 +50,69 @@ def conjugate(text, options):
     myconjugator = qutrub_api.QutrubApi(db_path = config.qutrub_config.DB_BASE_PATH)
     #extract first word if many words are given
     word = text.split(" ")[0]
-    
-    given_future_type = options.get("future_type",u"فتحة") 
-    # find future haraka for a given verb
-    verb_list = myconjugator.find_tri_verb(word, given_future_type)
-    # get vocalized form of the verb
-    if(verb_list):
-        word = verb_list[0].get("verb",word)
-        future_type = verb_list[0].get("haraka",word)
-        # ~ logging.debug("adaat.py:", future_type, verb_list[0].get("haraka",word))
-    else:
-        future_type = given_future_type
-    
-    conjugate_result =  do_sarf(myconjugator, word, 
-        future_type = future_type,
-        all         = options.get("all", False),
-        past        = options.get("past", False),
-        future      = options.get("future", False),
-        passive     = options.get("passive", False),
-        imperative  = options.get("imperative", False),
-        future_moode= options.get("future_moode", False),
-        confirmed   = options.get("confirmed", False),
-        transitive  = options.get("transitive", False),
-        );
-        
-    conjugate_result_table = conjugate_result.get("table",{})
-    if(not conjugate_result_table):
-        conjugate_result_suggest = conjugate_result.get("suggest",{})
-    else:
-        conjugate_result_suggest = myconjugator.suggest_similar_verb_list(word, future_type)
-        
-    conjugate_result_verb_info= myconjugator.format_verb_info(conjugate_result.get("verb_info",""), bool(verb_list))
-
-    return {"table":conjugate_result_table,
-    "suggest":conjugate_result_suggest,
-    "verb_info":conjugate_result_verb_info}
-
-def do_sarf(myconjugator, word,future_type,all=True,past=False,future=False,passive=False,imperative=False,future_moode=False,confirmed=False,transitive=False,display_format="HTML"):
-    
-    # ~ from . import qutrub_api
-    
-    # ~ myconjugator = qutrub_api.QutrubApi(db_path = config.qutrub_config.DB_BASE_PATH)
-        
+    # if the verb is not valid:
     valid = myconjugator.is_valid_infinitive(word)
-    listetenses=[];
-    if valid:
-        future_type= myconjugator.get_future_type_by_name(future_type);
-        bab_sarf=0;
-        myconjugator.input(word, future_type, transitive);
-
-        myconjugator.set_display_format(display_format);
-    #test the uniformate function
-        listetenses= myconjugator.manage_tenses(all, past,future,passive,imperative,future_moode,confirmed,transitive)    
-
-        result =  myconjugator.conjugate_all_tenses(listetenses);
-        conjs_table = myconjugator.display()
-        verb_info= myconjugator.get_verb_info(word, future_type,  transitive )
-
-        return {"table":conjs_table,"suggest":[], "verb_info":verb_info}
-        
-    else:
-        suggestions  =  myconjugator.suggest_verb(word)
+    if not valid:    
+        suggestions  =  myconjugator.suggest_similar_verb_list(word, u"فتحة")
         if suggestions:
-            # ~ print("do-sarf",suggestions)
             return {"table":[], "verb_info":"","suggest":suggestions};
         else:
             return {"table":[], "verb_info":"","suggest":[]}
+    else:  # if verb is valid
             
-# ~ def suggest_similar_verb_list(text, options):
-    # ~ from . import qutrub_api
-    # ~ myconjugator = qutrub_api.QutrubApi(db_path = config.qutrub_config.DB_BASE_PATH)
-    # ~ return myconjugator.suggest_verb_list(text, options)
+        
+        given_future_type = options.get("future_type",u"فتحة") 
+        # find future haraka for a given verb
+        verb_list = myconjugator.find_tri_verb(word, given_future_type)
+        # get vocalized form of the verb
+        if(verb_list):
+            word = verb_list[0].get("verb",word)
+            future_type = verb_list[0].get("haraka",word)
+            # ~ logging.debug("adaat.py:", future_type, verb_list[0].get("haraka",word))
+        else:
+            future_type = given_future_type
+        
+        conjugate_result =  do_sarf(myconjugator, word, 
+            future_type = future_type,
+            all         = options.get("all", False),
+            past        = options.get("past", False),
+            future      = options.get("future", False),
+            passive     = options.get("passive", False),
+            imperative  = options.get("imperative", False),
+            future_moode= options.get("future_moode", False),
+            confirmed   = options.get("confirmed", False),
+            transitive  = options.get("transitive", False),
+            );
+            
+        conjugate_result_table = conjugate_result.get("table",{})
+        # more suggestion
+        conjugate_result_suggest = myconjugator.suggest_similar_verb_list(word, future_type)
+            
+        conjugate_result_verb_info= myconjugator.format_verb_info(conjugate_result.get("verb_info",""), bool(verb_list))
+
+        return {"table":conjugate_result_table,
+        "suggest":conjugate_result_suggest,
+        "verb_info":conjugate_result_verb_info}
+
+def do_sarf(myconjugator, word,future_type,all=True,past=False,future=False,passive=False,imperative=False,future_moode=False,confirmed=False,transitive=False,display_format="HTML"):
+    
+
+    future_type= myconjugator.get_future_type_by_name(future_type);
+    bab_sarf=0;
+    myconjugator.input(word, future_type, transitive);
+
+    myconjugator.set_display_format(display_format);
+    
+    listetenses= myconjugator.manage_tenses(all, past,future,passive,imperative,future_moode,confirmed,transitive)    
+
+    result =  myconjugator.conjugate_all_tenses(listetenses);
+    conjs_table = myconjugator.display()
+    verb_info= myconjugator.get_verb_info(word, future_type,  transitive )
+
+    return {"table":conjs_table, "verb_info":verb_info}
+        
+            
+
     
 
 
